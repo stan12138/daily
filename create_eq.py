cc9d7fe88a01ca073e3ade2873bbdc6ca0dd8826
stan <ihnyi@qq.com>
2017-10-14 20:58:04 +0800

    17.10.14

--- create_eq.py
+++ create_eq.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot as plt
 from random import choice
+from numpy import array
+import matplotlib
 
 __all__=['create']
 
@@ -7,11 +9,16 @@ def create(aim,size=35,name=None,name_pre=None) :
 	namelist=list(range(65,123))
 	if type(aim)==str :
 		fig = plt.figure()
-		fig.text(0.5,0.5,aim,fontsize=size,ha='center',va='center')
+		text = fig.text(0.5,0.5,aim,fontsize=size,ha='center',va='center')
+		plt.show()
+		box = text.get_window_extent()
+		plt.close()
+		point = box.get_points()+array([[-5,-5],[5,5]])
+		bbox = matplotlib.transforms.Bbox(point/fig.dpi)
 		if name :
-			plt.savefig(name,format='png',transparent=True,dpi=600)
+			fig.savefig(name,format='png',transparent=True,bbox_inches=bbox,dpi=600)
 		else :
-			plt.savefig(chr(choice(namelist))+chr(choice(namelist))+chr(choice(namelist))+'.png',format='png',transparent=True,dpi=600)
+			fig.savefig(chr(choice(namelist))+chr(choice(namelist))+chr(choice(namelist))+'.png',format='png',bbox_inches=bbox,transparent=True,dpi=600)
 		plt.close()
 	elif type(aim)==list or type(aim)==tuple :
 		j = 0
@@ -19,11 +26,16 @@ def create(aim,size=35,name=None,name_pre=None) :
 			name_pre = chr(choice(namelist))+chr(choice(namelist))+chr(choice(namelist))
 		for i in aim :
 			fig = plt.figure()
-			fig.text(0.5,0.5,i,fontsize=size,ha='center',va='center')
+			text = fig.text(0.5,0.5,i,fontsize=size,ha='center',va='center')
+			#plt.show()
+			box = text.get_window_extent(fig.canvas.get_renderer())
+			#plt.close()
+			point = box.get_points()+array([[-5,-5],[5,5]])
+			bbox = matplotlib.transforms.Bbox(point/fig.dpi)
 			if name and len(name)==len(aim) :
-				plt.savefig(name[j],format='png',transparent=True,dpi=600)
+				fig.savefig(name[j],format='png',transparent=True,bbox_inches=bbox,dpi=600)
 			else :
-				plt.savefig(name_pre+str(j)+'.png',format='png',transparent=True,dpi=600)
+				fig.savefig(name_pre+str(j)+'.png',format='png',transparent=True,bbox_inches=bbox,dpi=600)
 			j += 1
 			plt.close()
 
