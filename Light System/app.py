ee8780ee1199ad0264ce5c76640665a8d5fab2cd
stan <ihnyi@qq.com>
2019-01-30 18:27:16 +0800

    19.1.30

--- Light System/app.py
+++ Light System/app.py
@@ -1,6 +1,14 @@
 from flask import Flask,render_template,request,Response,session,escape,redirect,url_for
+import serial
+import time
 
 app = Flask(__name__)
+
+s=serial.Serial("/dev/ttyACM0", 9600, timeout=5)
+
+def send_message(x) :
+	pass
+	s.write(str(x).encode("utf-8"))
 
 @app.route('/',methods=['GET',])
 def home() :
@@ -8,13 +16,13 @@ def home() :
 
 @app.route("/b1", methods=['POST',])
 def b1() :
-	print("get b1....")
-	return 1
+	send_message(1)
+	return render_template("home.html")
 
 @app.route('/b2', methods=['POST',])
 def b2() :
-	print("this is b2")
-
+	send_message(0)
+	return render_template("home.html")
 
 if __name__ == "__main__" :
-	app.run(host="0.0.0.0",port=8000)
+	app.run(host="127.0.0.1",port=5000)
--- Light System/static/home.js
+++ Light System/static/home.js
@@ -7,8 +7,21 @@ b2.onclick = b2_ajax;
 var httpRequest_b1;
 var httpRequest_b2;
 
+var b1_on = true;
+var b2_on = true;
+
 function b1_ajax(e)
 {
+    if(b1_on)
+    {
+        b1_on = false;
+        b1.className = "button open";
+    }
+    else
+    {
+        b1_on = true;
+        b1.className = "button close";
+    }
     var form = document.getElementById("b1-form");
     
     var form_data = new FormData(form);
@@ -28,6 +41,16 @@ function ajax_handle_response() {
 
 function b2_ajax(e)
 {
+    if(b2_on)
+    {
+        b2_on = false;
+        b2.className = "button open";
+    }
+    else
+    {
+        b2_on = true;
+        b2.className = "button close";
+    }
     var form = document.getElementById("b2-form");
     
     var form_data = new FormData(form);
